94187c12ad9c8308949ca91df8d8001cae40e3ca
Mrunali Sanjay Khandat <monakhandat@gmail.com>
2019-02-23 23:54:17 -0800

    Adding screenshots

--- lab3/observer/__SHELL14.java
+++ lab3/observer/__SHELL14.java
@@ -1,5 +1,5 @@
 package observer;
-public class __SHELL13 extends bluej.runtime.Shell {
+public class __SHELL14 extends bluej.runtime.Shell {
 public static void run() throws Throwable {
 
 java.lang.String[] __bluej_param0 = { };
