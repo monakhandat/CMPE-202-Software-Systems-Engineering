6be8ea132ff3f97a2a8ab4fa939cd43296487e38
Mrunali Sanjay Khandat <monakhandat@gmail.com>
2019-04-06 21:25:05 -0700

    Lab 8 basic

---
+++ lab8/inputmask/src/main/java/CreditCardExp.java
@@ -0,0 +1,36 @@
+/* (c) Copyright 2018 Paul Nguyen. All Rights Reserved */
+package inputmask ;
+
+public class CreditCardExp implements IDisplayComponent, IKeyEventHandler
+{
+
+	private IKeyEventHandler nextHandler ;
+	private String date = "" ;
+
+    public void setNext( IKeyEventHandler next) {
+    	this.nextHandler = next ;
+    }	
+
+	public String display() {
+		if ( date.equals("") )
+			return "[MM/YY]" + "  " ;
+		else
+			return "[" + date + "]" + "  " ;
+	}	
+
+	public void key(String ch, int cnt) {
+		if ( cnt >= 17 && cnt <= 20  )
+		{
+			date += ch ;
+			if (date.length() == 2)
+				date += "/";
+		}
+		else if ( nextHandler != null )
+			nextHandler.key(ch, cnt) ;
+	}	
+
+	public void addSubComponent( IDisplayComponent c ) {
+		return ; // do nothing
+	}
+
+}
